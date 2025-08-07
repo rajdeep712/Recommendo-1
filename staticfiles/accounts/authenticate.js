aec972a0fdf94096066458ee7c61c2b0fe8a9506
rajdeep712 <rajdeeppa53@gmail.com>
2025-08-08 02:34:24 +0530

    delete static files folder

--- staticfiles/accounts/authenticate.js
+++
@@ -1,7 +0,0 @@
-const gif = document.querySelector('.gif');
-const instruction = document.querySelector('.instruction');
-
-setTimeout(() => {
-    gif.style.display = 'none';
-    instruction.classList.add('animate');
-},1500)
